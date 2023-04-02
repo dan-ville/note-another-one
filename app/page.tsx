4872fb4b769e7245bdc94ad8043b40e4d9ea07ae
Daniel Villegas <daniel.villegas@fsgi.com>
2023-04-02 10:25:20 -0400

    new note functionality

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,18 @@ import { useState } from "react"
 import { Note as NoteInterface } from "./types"
 import Note from "./components/Note"
 
+const formInitialValues = {
+  title: "",
+  content: "",
+}
 export default function Home() {
   const [notes, setNotes] = useState<NoteInterface[]>([])
+  const [form, setForm] = useState(formInitialValues)
+  const onInputChange = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setForm({ ...form, [event.target.name]: event.target.value })
+  }
 
   const updateNote = (id: string) => (note: Partial<NoteInterface>) => {
     const target = notes.find((note) => note.id == id)
@@ -12,11 +22,32 @@ export default function Home() {
     setNotes(() => notes.map((x) => (x.id == id ? updated : x)))
   }
 
+  const saveNewNote = () => {
+    const newNote = { ...form, id: (Math.random() * 10000).toString() }
+    setNotes((prev) => [...prev, newNote])
+    setForm(formInitialValues)
+  }
 
   return (
     <main>
       <div className="notes-grid">
-        
+        <div className="note-container">
+          <input
+            type="text"
+            placeholder={form.title}
+            name="title"
+            value={form.title}
+            onChange={onInputChange}
+          />
+          <textarea
+            rows={5}
+            defaultValue={form.content}
+            name="content"
+            value={form.content}
+            onChange={onInputChange}
+          />
+          <button onClick={saveNewNote}>Save</button>
+        </div>
         {notes.map((note, key) => (
           <Note key={key} note={note} updateNote={updateNote} />
         ))}
